80dd1056b0fd74ce73de502e3ff76c60fd847e1a
mackelec <mackelec@users.noreply.github.com>
2023-10-02 15:47:57 +1000

    Add files via upload

--- examples/hello_world.ino
+++ examples/hello_world.ino
@@ -1,1 +1,16 @@
+#include "SerialQueueTrickler.h"
 
+SerialQueueTrickler buffer(Serial1, 64); // Now using Serial1
+
+void setup() {
+  Serial1.begin(9600);
+}
+
+void loop() {
+  buffer.enqueue((const byte*)"Hello World!"); // Enqueue string up to '\0'
+  //buffer.enqueue(bufferArray, 0); // Enqueue whole buffer as binary
+  //buffer.enqueue(bufferArray, 10); // Enqueue first 10 bytes of buffer
+
+  buffer.send();
+  delay(2000);
+}
